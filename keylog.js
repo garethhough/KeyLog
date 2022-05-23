8188bc6bbcc0854a48a799731342d5e3e86f092f
garethhough <garethhough@googlemail.com>
2022-05-23 16:44:31 +0100

    Adjusted size and added event code

--- keylog.js
+++ keylog.js
@@ -1,8 +1,11 @@
 const displayKey = document.querySelector(".key h3");
 const displayKeyCode = document.querySelector(".keyCode h3");
+const displayEventCode = document.querySelector(".eventCode h3");
+
 
 window.addEventListener("keydown", (e) => {
     displayKey.innerText = e.key;
+    displayEventCode.innerText = e.code;
     displayKeyCode.innerText = e.keyCode;
     if (e.keyCode === 32) {
         displayKey.innerText = "Space Bar";
