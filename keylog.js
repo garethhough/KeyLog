f70aef581ce6e51907fd419b88ffe451bbbb24fe
garethhough <garethhough@googlemail.com>
2022-05-23 15:35:50 +0100

    removed trial code

--- keylog.js
+++ keylog.js
@@ -1,6 +1,5 @@
 const displayKey = document.querySelector(".key h3");
 const displayKeyCode = document.querySelector(".keyCode h3");
-const keyCodeDive = document.querySelector(".keyCode");
 
 window.addEventListener("keydown", (e) => {
     displayKey.innerText = e.key;
